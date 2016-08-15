612a095427c886d5daeeb05fb320e0af400c6722
dylan <dylan.kojicheslin@gmail.com>
2016-08-14 21:28:23 -0600

    cruft

--- imports/api/Cards/server/publications.js
+++ imports/api/Cards/server/publications.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import { Cards } from '../Cards.js';
 
 Meteor.publish('cardListPublication', function cardListPublicationFunciton() {
-  return Cards.find({}, {});
+  return Cards.find();
 });
--- imports/ui/components/cardList.js
+++ imports/ui/components/cardList.js
@@ -1,8 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Mongo } from 'meteor/mongo';
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { Tracker } from 'meteor/tracker';
 import { Cards } from '../../api/Cards/Cards.js';
 import './cardList.html';
 
