b7aa3caaa368e3bbe76d2c7fe2331cf0276c5b69
dylan <dylan.kojicheslin@gmail.com>
2016-09-15 17:12:51 -0600

    removed composite folder

--- imports/startup/client/routes.app-test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env mocha */
-
-// import { Meteor } from 'meteor/meteor';
-// import { Tracker } from 'meteor/tracker';
-// import { DDP } from 'meteor/ddp-client';
-// import { FlowRouter } from 'meteor/kadira:flow-router';
-// import { assert } from 'meteor/practicalmeteor:chai';
-// import { Promise } from 'meteor/promise';
-// import { $ } from 'meteor/jquery';
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,4 +1,3 @@
 import '../../api/Cards/methods.js';
 import '../../api/Cards/server/publications.js';
 import '../../api/Tags/server/publications.js';
-import '../../api/composite/server/publications.js'
