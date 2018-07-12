57d4ec15effa09c603e99655db9961d8ab62bc8f
dylan <dylan.kojicheslin@gmail.com>
2018-07-12 01:54:41 -0600

    WIP commit

--- imports/api/Cards/server/publications.js
+++ imports/api/Cards/server/publications.js
@@ -4,3 +4,38 @@ import { Cards } from '../Cards.js';
 Meteor.publish('cardsListPublication', function cardsListPublicationFunciton() {
   return Cards.find();
 });
+
+Meteor.publishComposite('cardWithTags', function() {
+  //validate input
+  new SimpleSchema({
+    listId: {type: String}
+  }).validate({ listId });
+
+  const userId = this.userId;
+
+ //card query obj
+  return {
+    find() {
+      const query = {};
+
+      const options = {
+        fields: {
+          limit: 10,
+          fields: {
+            {question:1},
+            {answer:1},
+            {listTags:1},
+          }
+         }
+      };
+
+      return Cards.find(query, options);
+    },
+    // tags
+    children: [{
+      find(card) {
+        return Tags.find({ listId: list._id }, { fields: Todos.publicFields });
+      }
+    }]
+  };
+});
